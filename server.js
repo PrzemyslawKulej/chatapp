415d21aed13d7ee7db72beb6e58322eeafbf8759
Przemyslaw Kulej <przemek.kulej1@gmail.com>
2024-04-30 02:00:52 +0200

    Created server

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const net = require("net");
 
 const server = net.createServer();
 
+server.on("connection", (socket) => {
+    
+})
+
 server.listen(3008, "127.0.0.1", () => {
     console.log("Opened server on", server.address)
-    console.log("Server working...")
 })
